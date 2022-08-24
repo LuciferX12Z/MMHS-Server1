3324b79e4083fe06f04539b3bd1bd7961f7f4895
LuciferX12Z <61372682+LuciferX12Z@users.noreply.github.com>
2022-08-24 13:31:03 +0630

    logout fixing

--- middlewares/checkLoggedIn.js
+++ middlewares/checkLoggedIn.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = {
   checkLoggedIn: (req, res, next) => {
     const token = req.cookies.token;
+    console.log(token);
     if (!token) {
       return res.status(401).json({
         message: "You are not logged in",
