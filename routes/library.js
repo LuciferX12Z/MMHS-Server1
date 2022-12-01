293da1d4212c226dd716702bae969e16a4835be7
ZinMinHtet21801 <zetminhtin@gmail.com>
2022-12-02 01:08:18 +0630

    added addBook editbook and deletebook route and function

--- routes/library.js
+++ routes/library.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 const { verifyBookData } = require("../verifyData");
 const { booksModel } = require("../database/Models");
-const cloudinary = require("cloudinary").v2;
 const { ObjectId } = require("mongodb");
 const { checkLoggedIn } = require("../middlewares/checkLoggedIn");
-const { uploadToCloudinary } = require("./courses");
+const uploadToCloudinary = require("../helpers/uploadImageToCloudinary");
 
 router.post("/addBook", checkLoggedIn, async (req, res) => {
   const {
@@ -15,6 +14,7 @@ router.post("/addBook", checkLoggedIn, async (req, res) => {
     details,
     publisher,
     publish_date,
+    bookUrl,
   } = req.body;
   const bookData = await verifyBookData(
     book_name,
@@ -22,7 +22,8 @@ router.post("/addBook", checkLoggedIn, async (req, res) => {
     category,
     details,
     publisher,
-    publish_date
+    publish_date,
+    bookUrl
   ).then((value) => value);
   if (!bookData) {
     return res.status(400).json({ message: "Error:Wrong type of Data!" });
@@ -31,8 +32,7 @@ router.post("/addBook", checkLoggedIn, async (req, res) => {
     const bookCheck = await booksModel.findOne({ book_name: book_name }).exec();
     if (!bookCheck) {
       let image = [];
-      image = await uploadToCloudinary(courseImageUpload);
-
+      image = uploadToCloudinary(bookImageUpload);
       const books = new booksModel({
         bookImageUpload,
         book_name,
@@ -41,6 +41,7 @@ router.post("/addBook", checkLoggedIn, async (req, res) => {
         details,
         publisher,
         publish_date,
+        bookUrl,
       });
       await books.save();
       return res.json({ message: "Success:Book created successfully!" });
@@ -51,70 +52,72 @@ router.post("/addBook", checkLoggedIn, async (req, res) => {
   }
 });
 
-// router.put("/editCourse/:id", checkLoggedIn, async (req, res) => {
-//   const { id } = req.params;
-//   const {
-//     courseImageUpload,
-//     courseName,
-//     teacher,
-//     studentLimit,
-//     fee,
-//     startingDate,
-//     endingDate,
-//     details,
-//   } = req.body;
-//   const classData = await verifyClassData(
-//     courseName,
-//     details,
-//     endingDate,
-//     fee,
-//     startingDate,
-//     studentLimit,
-//     teacher
-//   ).then((value) => value);
-//   if (!classData) {
-//     return res.status(400).json({ message: "Error:Wrong type of Data!" });
-//   }
+router.put("/editBook/:id", checkLoggedIn, async (req, res) => {
+  const { id } = req.params;
+  const {
+    bookImageUpload,
+    book_name,
+    author,
+    category,
+    details,
+    publisher,
+    publish_date,
+    bookUrl,
+  } = req.body;
+  const bookData = await verifyBookData(
+    book_name,
+    author,
+    category,
+    details,
+    publisher,
+    publish_date,
+    bookUrl
+  ).then((value) => value);
+  if (!bookData) {
+    return res.status(400).json({ message: "Error:Wrong type of Data!" });
+  }
 
-//   try {
-//     const courseCheck = await classesModel
-//       .findOne({ _id: ObjectId(id) })
-//       .exec();
-//     if (courseCheck) {
-//       let image;
-//       image = await uploadToCloudinary(courseImageUpload);
-//       const classes = await classesModel.updateOne(
-//         { _id: ObjectId(id) },
-//         {
-//           courseImageUpload: image,
-//           courseName: courseName,
-//           details: details,
-//           endingDate: endingDate,
-//           fee: fee,
-//           startingDate: startingDate,
-//           studentLimit: studentLimit,
-//           teacher: teacher,
-//         }
-//       );
-//       return res
-//         .status(200)
-//         .json({ message: "Success:Course updated successfully!" });
-//     }
-//     return res.status(400).json({ message: "Error:Course already exists!" });
-//   } catch (e) {
-//     console.log(e);
-//   }
-// });
+  try {
+    const bookCheck = await booksModel.findOne({ _id: ObjectId(id) }).exec();
+    if (bookCheck) {
+      let image;
+      image = await uploadToCloudinary(bookImageUpload);
+      const book = await booksModel.updateOne(
+        { _id: ObjectId(id) },
+        {
+          bookImageUpload,
+          book_name,
+          author,
+          category,
+          details,
+          publisher,
+          publish_date,
+          bookUrl,
+        }
+      );
+      return res
+        .status(200)
+        .json({ message: "Success:Course updated successfully!" });
+    }
+    return res.status(400).json({ message: "Error:Book does not exist!" });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
-// router.delete("/deleteCourse/:id", async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     const classes = await classesModel.deleteOne({ _id: ObjectId(id) });
-//     return res
-//       .status(200)
-//       .json({ message: "Success:Course deleted successfully!" });
-//   } catch (e) {
-//     console.log(e);
-//   }
-// });
-// module.exports = router;
+router.delete("/deleteBook/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const bookCheck = await booksModel.findOne({ _id: ObjectId(id) }).exec();
+    if (bookCheck) {
+      const book = await booksModel.deleteOne({ _id: ObjectId(id) });
+      return res
+        .status(200)
+        .json({ message: "Success:Course deleted successfully!" });
+    }
+    return res.status(400).json({ message: "Error:Book does not exist!" });
+  } catch (e) {
+    console.log(e);
+  }
+});
+module.exports = router;
