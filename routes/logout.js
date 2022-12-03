7520f9964ce0f11a301b9d56f8e0c641a9b23446
LuciferX12Z <khantkoaung1200@gmail.com>
2022-12-03 15:06:40 +0630

    login fixed

--- routes/logout.js
+++ routes/logout.js
@@ -1,18 +1,23 @@
 const router = require("express").Router();
+const isProdMode = process.env.NODE_ENV === "production";
+
+const CORS_FRONT_END_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.PRODUCTION_FRONTEND_URL
+    : process.env.DEVELOPMENT_FRONTEND_URL;
 router.post("/", (req, res) => {
   console.log(req.cookies.token);
   res
     .clearCookie("token", {
-      domain: "https://mmhs-client1.vercel.app",
+      domain: CORS_FRONT_END_URL,
       path: "/",
-      httpOnly: true,
-      sameSite: "None",
-      secure: true,
+      httpOnly: isProdMode ? true : false,
+      sameSite: isProdMode ? "None" : "lax",
+      secure: isProdMode ? true : false,
     })
     .status(200)
     .json({
       message: "ok",
-      token: req.cookies.token,
     })
     .end();
 });
